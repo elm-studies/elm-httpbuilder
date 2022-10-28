db54e2e7fe6354850aab7c1d1cf4b98f24fff8f9
sxs <xx@x.com>
2022-10-29 00:39:15 +0200

    Update Main

--- src/Main.elm
+++ src/Main.elm
@@ -1,33 +1,63 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, text, div, h1, img)
-import Html.Attributes exposing (src)
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Http
+
 
 
 ---- MODEL ----
 
 
 type alias Model =
-    {}
+    { heading : String
+    , factText : String
+    , input : String
+    }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( {}, Cmd.none )
-
-
-
----- UPDATE ----
+    ( { heading = "Cat Facts"
+      , factText = "Click the button to get a cat fact"
+      , input = ""
+      }
+    , Cmd.none
+    )
 
 
 type Msg
-    = NoOp
+    = ShowFacts
+    | Input String
+    | NewFactArrived (Result Http.Error String)
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    ( model, Cmd.none )
+    case msg of
+        Input newInput ->
+            ( Model "NumbersApi typing.." "" newInput, Cmd.none )
+
+        ShowFacts ->
+            let
+                url =
+                    "https://jsonplaceholder.typicode.com/todos/" ++ model.input
+
+                sendRequest =
+                    Http.get
+                        { url = url
+                        , expect = Http.expectString NewFactArrived
+                        }
+            in
+            ( model, sendRequest )
+
+        NewFactArrived (Ok fact) ->
+            ( Model "NumbersApi" fact model.input, Cmd.none )
+
+        NewFactArrived (Err _) ->
+            ( Model model.input "not valid value" "", Cmd.none )
 
 
 
@@ -37,8 +67,11 @@ update msg model =
 view : Model -> Html Msg
 view model =
     div []
-        [ img [ src "/logo.svg" ] []
-        , h1 [] [ text "Your Elm App is working!" ]
+        [ h2 [] [ text model.heading ]
+        , input [ onInput Input, value model.input ] []
+        , button [ onClick ShowFacts ] [ text "show facts" ]
+        , br [] []
+        , h3 [] [ text model.factText ]
         ]
 
 
